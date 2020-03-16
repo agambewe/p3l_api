c258a943377967f36d8a57f9cda898a1bf797a1d
= <=>
2020-03-17 00:01:03 +0700

    update

--- app/Produk.php
+++ app/Produk.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Produk extends Model
 {
-    public $table = "pegawai";
+    public $table = "produk";
 
     protected $fillable = [
         'nama', 'satuan', 'stok', 'minimal', 'harga',
